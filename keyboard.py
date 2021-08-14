c3251c33a892540c4b4d95c985a48638f03764d4
Pavel <87094335+tabarincev@users.noreply.github.com>
2021-08-15 00:21:02 +0300

    PEP8 formatting

--- keyboard.py
+++ keyboard.py
@@ -11,7 +11,8 @@ class ReplyKeyboard:
         for i in range(len(faculties)):
             faculty_markup.row(*faculties[i])
         
-        self.bot.send_message(message.from_user.id, 'Выбери свой факультет.', reply_markup=faculty_markup)
+        self.bot.send_message(message.from_user.id, 'Выбери свой факультет.', 
+                              reply_markup=faculty_markup)
 
     def group_markup(self, message, groups_by_faculty):
         group_markup = telebot.types.ReplyKeyboardMarkup(True, False)
@@ -20,4 +21,14 @@ class ReplyKeyboard:
             group_markup.row(*groups_by_faculty[i])
         group_markup.row('Назад ⬅️')
 
-        self.bot.send_message(message.chat.id, 'Выбери группу.', reply_markup=group_markup)
+        self.bot.send_message(message.chat.id, 'Выбери группу.', 
+                              reply_markup=group_markup)
+
+    def display_schedule_markup(self, message):
+        display_markup = telebot.types.ReplyKeyboardMarkup(True, False)
+
+        display_markup.row('Сегодня 📅', 'Завтра ➡️')
+        display_markup.row('Назад ⬅️')
+
+        self.bot.send_message(message.chat.id, 'Выбери день.', 
+                              reply_markup=display_markup)
