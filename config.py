56b6c0c142a5b6ecef05f239770abeaad981fdb9
Pavel <87094335+tabarincev@users.noreply.github.com>
2021-09-06 19:03:22 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,17 @@
-token = 'insert your token'
+TOKEN = 'your token from @BotFather'
+URL = 'https://www.sut.ru/studentu/raspisanie/raspisanie-zanyatiy-studentov-ochnoy-i-vecherney-form-obucheniya'
+
+HEADER = {
+    'User-Agent': 
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_4) AppleWebKit/537.36 '
+                  '(KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36',
+    'Referer': 'https://www.sut.ru/'
+}
+
+db_params = {
+  'user': 'user_db',
+  'password': 'pass_db',
+  'host': 'host_db',
+  'port': '5432',
+  'database': 'name_db'
+}
